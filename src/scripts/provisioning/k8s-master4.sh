330cd7d9fb587e5a247aa15752389e5b77df6c52
Janusz Kujawa <janusz.kujawa@gmail.com>
2021-02-19 23:03:34 +0100

    Change directory for additional disks from extrastorage to extradisks, added VirtIO adapter

---
+++ src/scripts/provisioning/k8s-master4.sh
@@ -0,0 +1,11 @@
+cat <<EOF | tee /etc/motd
+.--- k8s lab ---.
+| Master Node 4 |
+'---------------'
+EOF
+
+#echo "::: INSTALL HELM SCRIPT WAY :::"
+#curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+#chmod 700 /tmp/get_helm.sh
+#/tmp/get_helm.sh
+#exit 0
